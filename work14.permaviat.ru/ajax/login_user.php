93c1cbc09fb2651e31bf95420102110866202826
farback <000shereposhek000@gmail.com>
2026-02-04 12:57:51 +0500

    блокировка учетной записи

--- work14.permaviat.ru/ajax/login_user.php
+++ work14.permaviat.ru/ajax/login_user.php
@@ -2,19 +2,59 @@
 	session_start();
 	include("../settings/connect_datebase.php");
 	
+	$NowDate = date("Y-m-d H:i:s");
+	$Sql = "SELECT * FROM `access_ip` WHERE `ip` = '$user_ip';";
+	$QueryAccess = $mysqli->query($Sql);
+	if($QueryAccess->num_rows > 0){
+		$ReadAccess = $QueryAccess->fetch_assoc();
+		$EndDate = $ReadAccess["endDate"];
+		$StartDate = $ReadAccess["startDate"];
+
+		if($StartDate==$EndDate){
+			echo md5(md5(-1));
+			exit;
+		}else{
+			$Sql = "UPDATE `access_ip` SET `startDate`='$EndDate', `endDate` = '$NowDate' WHERE `ip` = '$user_ip'";
+			$mysqli->query($Sql);
+		}
+	}else{
+		$Sql = "INSERT INTO `access_ip`(`ip`, `startDate`, `endDate`) VALUES ('$user_ip', NULL,'$NowDate')";
+		$mysqli->query($Sql);
+	}
+
 	$login = $_POST['login'];
 	$password = $_POST['password'];
 	
+	$CountAttempt = 0;
+	$Sql = "SELECT `attempt` FROM `users` WHERE `login`= '$login'";
+	$QueryAttempt = $mysqli->query($Sql);
+	if($QueryAttempt->num_rows>0){
+		$ReadAttempt = $QueryAttempt->fetch_assoc();
+		$CountAttempt = $ReadAttempt['attempt'];
+	}
+		
+	if($CountAttempt>=5){
+		echo md5(md5(-1 ));
+		exit;
+	}
+			
+	$id = -1;
 	// ищем пользователя
 	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
 	
-	$id = -1;
 	while($user_read = $query_user->fetch_row()) {
 		$id = $user_read[0];
 	}
 	
 	if($id != -1) {
 		$_SESSION['user'] = $id;
+		$CountAttempt = 0;
+	}else{
+		$CountAttempt++;
 	}
+
+	$Sql = "UPDATE `users` SET `attempt`= $CountAttempt WHERE `login`= '$login'";
+	$mysqli->query($Sql);
+
 	echo md5(md5($id));
 ?>
--- work14.permaviat.ru/settings/connect_datebase.php
+++ work14.permaviat.ru/settings/connect_datebase.php
@@ -1,3 +1,19 @@
 <?php
 	$mysqli = new mysqli('127.0.0.1', 'root', '', 'security');
+
+	function getClientIp(){
+		if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+			return $_SERVER['HTTP_CLIENT_IP'];
+		}elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+			$ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+			return trim($ips[0]);
+		}else{
+			return $_SERVER['REMOTE_ADDR'];
+		}
+	}
+
+	$user_ip = getClientIp();
+
+	setcookie("IP", $user_ip);
+	setcookie("DateTime", date("Y-m-d H:i:s"));
 ?>
